06583a533b8a987dd7296bcc18da14e3df8e1af2
uly5478 <uly.chen@gmail.com>
2026-03-21 11:37:38 +0900

    fix: handle missing port in postgres:// URL

--- src/ProcurementInventory.Api/Program.cs
+++ src/ProcurementInventory.Api/Program.cs
@@ -25,7 +25,8 @@ if (rawConnStr.StartsWith("postgres://") || rawConnStr.StartsWith("postgresql://
 {
     var uri = new Uri(rawConnStr);
     var userInfo = uri.UserInfo.Split(':');
-    rawConnStr = $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
+    var port = uri.Port > 0 ? uri.Port : 5432;
+    rawConnStr = $"Host={uri.Host};Port={port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
 }
 
 builder.Services.AddDbContext<AppDbContext>(options =>
